ddd03f41d2ee1dab5d79c58321fc14c5cd5cb346
epicgamer <68295724+IbrahimBilalPrograms@users.noreply.github.com>
2020-12-25 09:03:29 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Ibrahim Bilal',
-    description: 'I make coding more fun with code',
+    description:
       'Ibrahim Bilal is an epic gamer, coder and an avid reader he likes minecraft', 
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
